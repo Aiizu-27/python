b2891a55e1c81fc3e17a06bc5c5fc588f0f1619a
Aiizu <luciiiia.2713@gmail.com>
2025-10-07 23:58:40 +0200

    Update EjercicioAvanzado.py

--- EjercicioAvanzado.py
+++ EjercicioAvanzado.py
@@ -1,22 +1,24 @@
 materias_inscritas = int(input("Introduzca el número de materias a las que esté inscrito o inscrita: "))
 notas_materias = []
+
 for i in range(materias_inscritas):
     numero = i + 1
-    nota_materia = int(input(f"Introduzca la nota de la materia {numero}: "))
+    nota_materia = float(input(f"Introduzca la nota de la materia {numero}: "))
     notas_materias.append(nota_materia)
 
-print (notas_materias)
+print(notas_materias)
 
-media_notas = sum (notas_materias) / materias_inscritas
+media_notas = sum(notas_materias) / materias_inscritas
+
 if media_notas < 5:
-    print(float(f"Tu media es de {media_notas} y tienes un insuficiente"))
+    print(f"Tu media es de {media_notas:.2f} y tienes un insuficiente")
 elif media_notas < 6:
-    print(float(f"Tu media es de {media_notas} y tienes un suficiente"))
+    print(f"Tu media es de {media_notas:.2f} y tienes un suficiente")
 elif media_notas < 7.5:
-    print(float(f"Tu media es de {media_notas} y tienes un bien"))
+    print(f"Tu media es de {media_notas:.2f} y tienes un bien")
 elif media_notas < 9:
-    print(float(f"Tu media es de {media_notas} y tienes un notable"))
+    print(f"Tu media es de {media_notas:.2f} y tienes un notable")
 elif media_notas < 10:
-    print(float(f"Tu media es de {media_notas} y tienes un sobresaliente"))
-elif media_notas == 10:
-    print(float(f"Tu media es de {media_notas} y tienes un sobresaliente con una mención honorífica"))
+    print(f"Tu media es de {media_notas:.2f} y tienes un sobresaliente")
+else:  # media_notas == 10
+    print(f"Tu media es de {media_notas:.2f} y tienes un sobresaliente con mención honorífica")
